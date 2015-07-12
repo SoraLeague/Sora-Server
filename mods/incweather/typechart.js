ada4486ecf4ddb88be5774a673b674d45822ef95
Blazing360 <Rotflol360@gmail.com>
2015-07-12 14:18:10 +1000

    Create typechart.js

---
+++ mods/incweather/typechart.js
@@ -0,0 +1,56 @@
+exports.BattleTypeChart = {
+  "Poison": {
+    "damageTaken": {
+      psn: 3,
+      tox: 3,
+      acidrain: 3,
+      "Bug": 2,
+      "Dark": 0,
+      "Dragon": 0,
+      "Electric": 0,
+      "Fairy": 2,
+      "Fighting": 2,
+      "Fire": 0,
+      "Flying": 0,
+      "Ghost": 0,
+      "Grass": 2,
+      "Ground": 1,
+      "Ice": 0,
+      "Normal": 0,
+      "Poison": 2,
+      "Psychic": 1,
+      "Rock": 0,
+      "Steel": 0,
+      "Water": 0
+    },
+    "HPivs": {"def":30,"spa":30,"spd":30}
+  },
+  
+  "Steel": {
+    "damageTaken": {
+      psn: 3,
+      tox: 3,
+      sandstorm: 3,
+      acidrain: 3,
+      "Bug": 2,
+      "Dark": 0,
+      "Dragon": 2,
+      "Electric": 0,
+      "Fairy": 2,
+      "Fighting": 1,
+      "Fire": 1,
+      "Flying": 2,
+      "Ghost": 0,
+      "Grass": 2,
+      "Ground": 1,
+      "Ice": 2,
+      "Normal": 2,
+      "Poison": 3,
+      "Psychic": 2,
+      "Rock": 2,
+      "Steel": 2,
+      "Water": 0
+    },
+    "HPivs": {"spd":30}
+  }
+};
