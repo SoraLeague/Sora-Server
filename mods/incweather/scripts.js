c983ddc01c03bf44d3e976f2af3bea66a41f3a9f
Blazing360 <Rotflol360@gmail.com>
2015-07-12 14:17:02 +1000

    Create scripts.js

---
+++ mods/incweather/scripts.js
@@ -0,0 +1,126 @@
+exports.BattleScripts = {
+        inherit: 'gen6',
+        gen: 6,
+        init: function() {
+                //Pokemon that can learn Contaminate
+                this.modData('Learnsets', 'bulbasaur').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'ivysaur').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'venusaur').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'ekans').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'arbok').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'oddish').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'gloom').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'vileplume').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'bellsprout').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'weepinbell').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'victreebel').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'grimer').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'muk').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'koffing').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'weezing').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'nidoranf').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'nidorina').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'nidoqueen').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'qwilfish').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'gulpin').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'swalot').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'budew').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'roselia').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'roserade').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'stunky').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'skuntank').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'drapion').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'croagunk').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'toxicroak').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'trubbish').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'garbodor').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'foongus').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'amoonguss').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'skrelp').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'dragalge').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'shedinja').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'inkay').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'malamar').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'corphish').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'crawdaunt').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'absol').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'dratini').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'dragonair').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'dragonite').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'horsea').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'seadra').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'kingdra').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'goomy').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'sliggoo').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'goodra').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'torkoal').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'gyarados').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'tornadus').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'thundurus').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'landorus').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'yveltal').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'chikorita').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'bayleef').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'meganium').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'lileep').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'cradily').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'wooper').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'quagsire').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'barboach').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'whiscash').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'gastrodon').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'seismitoad').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'castform').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'slowpoke').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'slowbro').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'slowking').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'lugia').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'politoed').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'goldeen').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'seaking').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'octillery').learnset.contaminate = ['5M'];
+                
+                //Pokemon that have Contamination
+                this.modData('Pokedex', 'koffing').abilities['1'] = 'Contamination';
+                this.modData('Pokedex', 'weezing').abilities['1'] = 'Contamination';
+                
+                //Pokemon that have Acid Rush
+                this.modData('Pokedex', 'weedle').abilities['1'] = 'Acid Rush';
+                this.modData('Pokedex', 'beedrill').abilities['1'] = 'Acid Rush';
+                this.modData('Pokedex', 'stunky').abilities['H'] = 'Acid Rush';
+                this.modData('Pokedex', 'skuntank').abilities['H'] = 'Acid Rush';
+                this.modData('Pokedex', 'bellsprout').abilities['1'] = 'Acid Rush';
+                this.modData('Pokedex', 'weepinbell').abilities['1'] = 'Acid Rush';
+                this.modData('Pokedex', 'victreebel').abilities['1'] = 'Acid Rush';
+                
+                //Pokemon that have Chemical Regeneration
+                this.modData('Pokedex', 'oddish').abilities['1'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'gloom').abilities['1'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'vileplume').abilities['1'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'grimer').abilities['0'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'muk').abilities['0'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'foongus').abilities['1'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'amoonguss').abilities['1'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'goomy').abilities['0'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'sliggoo').abilities['0'] = 'Chemical Regeneration';
+                this.modData('Pokedex', 'goodra').abilities['0'] = 'Chemical Regeneration';
+                
+                //Pokemon that have High Molar
+                this.modData('Pokedex', 'ekans').abilities['H'] = 'High Molar';
+                this.modData('Pokedex', 'arbok').abilities['H'] = 'High Molar';
+                this.modData('Pokedex', 'spinarak').abilities['1'] = 'High Molar';
+                this.modData('Pokedex', 'ariados').abilities['1'] = 'High Molar';
+                this.modData('Pokedex', 'dustox').abilities['1'] = 'High Molar';
+                this.modData('Pokedex', 'trubbish').abilities['H'] = 'High Molar';
+                this.modData('Pokedex', 'garbodor').abilities['H'] = 'High Molar';
+                
+                //Pokemon that have Toxic Overdrive
+                this.modData('Pokedex', 'paras').abilities['H'] = 'Toxic Overdrive';
+                this.modData('Pokedex', 'parasect').abilities['H'] = 'Toxic Overdrive';
+                this.modData('Pokedex', 'zubat').abilities['1'] = 'Toxic Overdrive';
+                this.modData('Pokedex', 'golbat').abilities['1'] = 'Toxic Overdrive';
+                this.modData('Pokedex', 'crobat').abilities['1'] = 'Toxic Overdrive';
+                this.modData('Pokedex', 'gulpin').abilities['H'] = 'Toxic Overdrive';
+                this.modData('Pokedex', 'swalot').abilities['H'] = 'Toxic Overdrive';
+        }
+};
