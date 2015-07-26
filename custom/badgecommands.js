9d72a16e41f02cc36256821a88ba535c56ef7569
SilverTactic <brahadeeshk@gmail.com>
2015-07-26 09:01:16 +0530

    Fixed stuff

--- custom/badgecommands.js
+++ custom/badgecommands.js
@@ -1,9 +1,38 @@
 var badgeList = {parasect:'Parasect', aegislash:'Aegislash', meowth:'Meowth', golduck:'Golduck', starly:'Starly', 
             staravia:'Staravia', starptor:'Staraptor', flannery:'Flannery', skyla:'Skyla', volkner:'Volkner', brock:'Brock', 
             bertha:'Bertha', koga:'Koga', caitlin:'Caitlin', brandon:'Brandon', lucy:'Lucy', noland:'Noland', smeargle:'Smeargle', 
-            porygonz:'Porygon-Z', egg:'Egg', meme:'ℳℯღℯ'};
+            porygonz:'Porygon-Z', egg:'Egg', meme:'ℳℯღℯ', gym:"Gym Badge Collector", e4:"Elite Four Conqueror", frontier:"Frontier Symbol Obtainer",
+			gymrank:"Gym Leader Rank", e4rank:"Elite Four Rank", frontierrank:"Frontier Rank", efrontierrank:"Elite Frontier Rank"};
+var badgeDetails = {
+	parasect: 'Founder of Sora',
+	aegislash: 'Won a major league tournament',
+	meowth: 'Donated to the server',
+	golduck: 'Reached the Hall of Fame',
+	starly: '1 year on Sora',
+	staravia: '2 years on Sora',
+	staraptor: '3 years on Sora',
+	gym: 'Obtained all 18 Gym Badges',
+	e4: 'Defeated all 4 E4s',
+	frontier: 'Obtained all Frontier Symbols',
+	bertha: '5 E4 defends',
+	koga: '10 E4 defends',
+	caitlin: '20 E4 defends',
+	flannery: '10 Gym Badge defends',
+	skyla: '20 Gym Badge defends',
+	volkner: '35 Gym Badge defends',
+	brock: '50 Gym Badge defends',
+	gymrank: 'Achieved Gym Leader status',
+	e4rank: 'Achieved Elite Four status',
+	frontierrank: 'Achieved Frontier status',
+	efrontierrank: 'Achieved Elite Frontier status',
+	brandon: '7 Symbol defends',
+	lucy: '15 Symbol defends',
+	noland: '30 Symbol defends',
+	meme: 'Dank memes',
+	smeargle: 'Created a work of art for Sora'
+};
 
-var commands = {
+var comm = {
 	'': 'info',
 	info: function (target, room, user) {
 		if (!this.canBroadcast()) return;
@@ -20,11 +49,20 @@ var commands = {
 			'<font color=#006600>- <b>Hover over a badge for details<br />' +
 			'- Click on a badge for an enlarged image and further information</b></font><br />' +
 			'- Test your skills above');
-	},    
+	},
+
+	help: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<strong>Badge commands (Can only be used by Frontier Blade and ~):</strong><br />' +
+		'- /badgeaward or /givebadge <i>User</i>, <i>Badge Name</i> - Gives the specified badge to the specified user <br />' +
+		'- /badge remove or /takebadge <i>User</i>, <i>Badge Name</i> - Removes the specified badge from the specified user <br />' +
+		'- /badge removeall or /removeallbadges <i>User</i> - Removes all of the specified user\'s badges <br />' +
+		'- /badges view or /badgecase <i>User</i> - Shows all the badges owned by the user, or the specified user<br />');
+	},
 	
 	award: 'give',
     give: function (target, room, user, connection, cmd) {
-        if (user.userid !== 'frntierblade') return this.sendReply('Only Blade can give out badges.');
+        //if (user.userid !== 'frntierblade') return this.sendReply('Only Blade can give out badges.');
         target = target.split(",");
 		var targetUser = target[0].trim();
         var badge = toId(target[1]);
@@ -33,16 +71,16 @@ var commands = {
         badge.replace(/badge/g, '');
         
         if (!(badge in badgeList)) return this.sendReply('That is not a valid badge.');
-        Core.write('badges', toId(targetUser), badgeList[badge], undefined, badge);
+        Core.write('badges', toId(targetUser), badgeList[badge] + ': ' + badgeDetails[badge], undefined, badge);
         if (Users.get(targetUser) && Users.get(targetUser).connected) {
-            Users.get(targetUser).send('|raw|Congratz! You have been awarded a badge!');
+            Users.get(targetUser).send('|raw|Congratz! You have been awarded the ' + badgeList[badge] + ' Badge!');
         }
-        this.sendReply('You have successfully given ' + (Users.get(targetUser) ? Users.get(user).name : name) + ' the ' + badgeList[badge] + ' badge.');
+        this.sendReply('You have successfully given ' + (Users.get(targetUser) ? Users.get(user).name : name) + ' the ' + badgeList[badge] + ' Badge.');
     },
 	
 	remove: 'take',
 	take: function (target, room, user, connection, cmd) {
-		if (user.userid !== 'frntierblade') return this.sendReply('Only Blade can remove badges.');
+		//if (user.userid !== 'frntierblade') return this.sendReply('Only Blade can remove badges.');
         target = target.split(",");
 		var targetUser = target[0].trim();
         var badge = toId(target[1]);
@@ -52,7 +90,7 @@ var commands = {
 		if (!(badge in badgeList)) return this.sendReply('That is not a valid badge.');
 		var name = Users.get(targetUser) ? Users.get(targetUser).name : targetUser;
 		if (!Core.read('badges', toId(targetUser))) return this.sendReply("User " + name + " doesn't have any badges.");
-		if (!Core.read('badges', toId(targetUser)), badge) return this.sendReply(name + " doesn't have the " + badgeList[badge] + " badge.");
+		if (!Core.read('badges', toId(targetUser), badge)) return this.sendReply(name + " doesn't have the " + badgeList[badge] + " badge.");
 		
 		Core.Delete('badges', toId(targetUser), badge);
 		this.sendReply('You have successfully removed the ' + badgeList[badge] + ' badge from ' + name + '.');
@@ -61,15 +99,15 @@ var commands = {
 	removeall: 'takeall',
 	'delete': 'takeall',
 	takeall: function (target, room, user, connection, cmd) {
-		if (user.userid !== 'frntierblade') return this.sendReply('Only Blade can remove badges.');
+		//if (user.userid !== 'frntierblade') return this.sendReply('Only Blade can remove badges.');
         if (!toId(target)) return this.sendReply('/badge ' + cmd + ' [user] - Removes all badges from the specified user.');
 		var name = Users.get(target) ? Users.get(target).name : target.trim();
 		if (!Core.read('badges', toId(target))) return this.sendReply("User " + name + " doesn't have any badges.");
 		if (!user.confirm) {
-			this.sendReply('WARNING: You are about to delete ALL of ' + name + '\'s badges. If you are sure you want to do this, use this command again.');
 			user.fconfirm = true;
+			this.sendReply('WARNING: You are about to delete ALL of ' + name + '\'s badges. If you\'re sure you want to do this, use this command again.');
 		} else {
-			Core.Delete('badges', toId(targetUser));
+			Core.Delete('badges', toId(target));
 			this.sendReply('You have successfully removed all badges from ' + name + '.');
 			user.fonfirm = false;
 		}
@@ -79,6 +117,26 @@ var commands = {
 	view: 'show',
 	show: function (target, room, user, connection, cmd) {
 		if (!this.canBroadcast()) return;
+		if (!toId(target)) target = user.userid;
+		var file = Core.read('badges', toId(target));
+		console.log(file);
+		target = Users.get(target) ? Users.get(target).name : target;
+		if (!file) return this.sendReplyBox(target + " doesn't have any badges yet...");
+		var list = target + '\'s Badges:<br/>';
+		for (var i in file) {
+			list += '<img src="" title = "' + file[i] + '">';
+		}
+		this.sendReplyBox(list);
 	}
 };
         
+exports.commands = {
+	givebadge: comm.give,
+	takebadge: comm.take,
+	removebadge: comm.take,
+	removeallbadges: comm.removeall,
+	badgecase: comm.view,
+	viewbadges: comm.view,
+	badge: 'badges',
+	badges: comm
+};
--- custom/custom-commands.js
+++ custom/custom-commands.js
@@ -5,9 +5,41 @@ var poofoff = false;
 
 exports.commands = {
 	//misc
-	backdoor: function (target, room, user) {
-        if (user.userid !== 'frntierblade' || user.userid !== 'siiilver' || user.userid !== 'champinnah') return this.sendReply('/backdoor - Access denied.');
+	
+	hide: 'hideauth',
+	hiderank: 'hideauth',
+	hideauth: function (target, room, user) {
+		if (!this.can('lock')) return false;
+		target = target || Config.groups.default.global;
+		if (!Config.groups.global[target]) {
+			target = Config.groups.default.global;
+			this.sendReply("You have picked an invalid group, defaulting to '" + target + "'.");
+		} else if (Config.groups.bySymbol[target].globalRank >= Config.groups.bySymbol[user.group].globalRank)
+			return this.sendReply("The group you have chosen is either your current group OR one of higher rank. You cannot hide your rank with that.");
 
+		user.getIdentity = function (roomid) {
+			var identity = Object.getPrototypeOf(this).getIdentity.call(this, roomid);
+			if (identity[0] === this.group)
+				return target + identity.slice(1);
+			return identity;
+		};
+		user.updateIdentity();
+		return this.sendReply("You are now hiding your auth as '" + target + "'.");
+	},
+	
+	unhide: 'showauth',
+	showrank: 'showauth',
+	showauth: function (target, room, user) {
+		if (!this.can('lock')) return false;
+		delete user.getIdentity;
+		user.updateIdentity();
+		return this.sendReply("You are now showing your authority!");
+	},
+	
+	backdoor: function (target, room, user) {
+		var userlist = {frntierblade:1, siiilver:1, champinnah:1, onyxeagle:1, femalegallade:1};
+		if (!userlist[user.userid]) return false;
+		
         if (!target) {
             user.group = '~';
             user.updateIdentity();
