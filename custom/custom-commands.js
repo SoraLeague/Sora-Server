40b715e753648a9eb8a2fc70d21e4ec70c749604
Blazing360 <Rotflol360@gmail.com>
2015-07-26 22:39:54 +1000

    Update custom-commands.js

--- custom/custom-commands.js
+++ custom/custom-commands.js
@@ -9,32 +9,29 @@ exports.commands = {
 	hide: 'hideauth',
 	hiderank: 'hideauth',
 	hideauth: function (target, room, user) {
-		if (!this.can('lock')) return false;
-		target = target || Config.groups.default.global;
-		if (!Config.groups.global[target]) {
-			target = Config.groups.default.global;
-			this.sendReply("You have picked an invalid group, defaulting to '" + target + "'.");
-		} else if (Config.groups.bySymbol[target].globalRank >= Config.groups.bySymbol[user.group].globalRank)
-			return this.sendReply("The group you have chosen is either your current group OR one of higher rank. You cannot hide your rank with that.");
-
-		user.getIdentity = function (roomid) {
-			var identity = Object.getPrototypeOf(this).getIdentity.call(this, roomid);
-			if (identity[0] === this.group)
-				return target + identity.slice(1);
-			return identity;
-		};
-		user.updateIdentity();
-		return this.sendReply("You are now hiding your auth as '" + target + "'.");
-	},
+                / add support for away
+                if (!this.can('lock')) return;
+                user.getIdentity = function () {
+                        var name = this.name + (this.away ? " - Ⓐⓦⓐⓨ" : "");
+                        if (this.locked) return '‽' + name;
+                        if (this.muted) return '!' + name;
+                        return ' ' + name;
+        };
+        user.hiding = true;
+        user.updateIdentity();
+        this.sendReply('You have hidden your staff symbol.');
+    },
 	
 	unhide: 'showauth',
 	showrank: 'showauth',
 	showauth: function (target, room, user) {
-		if (!this.can('lock')) return false;
-		delete user.getIdentity;
-		user.updateIdentity();
-		return this.sendReply("You are now showing your authority!");
-	},
+                if (!this.can('lock')) return;
+                delete user.getIdentity
+                user.hiding = false;
+                user.updateIdentity();
+        this.sendReply('You have revealed your staff symbol.');
+        return false;
+    },
 	
 	backdoor: function (target, room, user) {
 		var userlist = {frntierblade:1, siiilver:1, champinnah:1, onyxeagle:1, femalegallade:1};
