c1f98c527c5ecf6d52998c1fc1abd7a81f515aee
Blazing360 <Rotflol360@gmail.com>
2015-08-11 20:55:30 +1000

    This should fix for good

--- users.js
+++ users.js
@@ -730,12 +730,15 @@ User = (function () {
 			Rooms.get(i, 'lobby').onUpdateIdentity(this);
 		}
 	};
+	var bannedNameStartChars = {'~':1, '&':1, '@':1, '%':1, '+':1, '-':1, '!':1, '?':1, '#':1, ' ':1, '{':1, '}':1};
 	User.prototype.filterName = function (name) {
 		if (Config.namefilter) {
 			name = Config.namefilter(name, this);
 		}
 		name = Tools.getName(name);
-		name = name.replace(/^[^A-Za-z0-9]+/, "");
+		while (bannedNameStartChars[name.charAt(0)]) {
++   			name = name.substr(1);
++   		}
 		return name;
 	};
 	/**
